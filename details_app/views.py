4e324f3756e6c7cb0ab6a0a42841fcc0e6144653
Sreenath k k <sreenathkk00@gmail.com>
2024-01-09 21:29:41 +0530

    nav bg-info,card btn-info

--- details_app/views.py
+++ details_app/views.py
@@ -1,19 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import ListView,CreateView
+from django.views.generic import ListView,CreateView,UpdateView
+from django.views import generic
 from .models import DetailsModel
 from .forms import DetailForm
 # Create your views here.
 
-
+# list the details 
 class DetailListview(ListView):
     model = DetailsModel
+    queryset = DetailsModel.objects.filter().all().order_by('-pub_date') 
+        
     template_name = 'index.html'
     context_object_name = 'Details_List'
+    
 
+# creat the details
 class FormView(CreateView):
     model = DetailsModel
     form_class = DetailForm
     template_name = 'form.html'
-    success_url ='/'
+    success_url ='/' 
 
+# update the details
+class UpdateView(UpdateView):
+    model = DetailsModel
+    
+
+print(dir(generic))
