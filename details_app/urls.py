11a5c791ad0da7c456f9086a2fa3d297517a7872
Sreenath k k <sreenathkk00@gmail.com>
2024-12-13 02:56:38 +0530

    Clear default comments and extra spaces, and format code for readability.

--- details_app/urls.py
+++ details_app/urls.py
@@ -1,33 +1,14 @@
-"""
-URL configuration for crud_project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.urls import path
 from details_app import views
-from .views import DetailListview,FormView,EditView,Update_View,Delete_View
+from .views import ListView,CreateView,EditView,UpdateView,DeleteView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    
-    path('',DetailListview.as_view(),name='detail_list'),
-    path('form/',FormView.as_view(),name='form_page'),
+    path('',ListView.as_view(),name='home'),
+    path('form/',CreateView.as_view(),name='Create_new'),
     path('edits/<int:pk>/',EditView.as_view(),name='edits'),
-    path('update_view/',Update_View.as_view(),name='update_view'),
-    path('delete_view/<int:pk>/',Delete_View.as_view(),name='delete')
+    path('update_view/',UpdateView.as_view(),name='update_view'),
+    path('delete_view/<int:pk>/',DeleteView.as_view(),name='delete')
 ]
-
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- details_app/views.py
+++ details_app/views.py
@@ -1,32 +1,28 @@
 from django.views.generic import ListView,CreateView,UpdateView,DeleteView
 from .models import DetailsModel
 from .forms import DetailForm
-import os
 from django.urls import reverse_lazy
-# Create your views here.
 
 # list the details 
-class DetailListview(ListView):
+class ListView(ListView):
     model = DetailsModel
     queryset = DetailsModel.objects.filter().all().order_by('-pub_date') 
-        
-    template_name = 'index.html'
+    form_class = DetailForm
+    template_name = 'home.html'
     context_object_name = 'Details_List'
+    success_url ='/' 
+    # add the form
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = self.form_class()
+        return context
     
-
 # creat the details
-class FormView(CreateView):
+class CreateView(CreateView):
     model = DetailsModel
     form_class = DetailForm
-    template_name = 'form.html'
-    success_url ='/' 
-
-
-# update_view will display the update.html page
-class Update_View(ListView):
-    model = DetailsModel
-    template_name = 'update.html'
-    context_object_name = 'update_view'
+    template_name = 'create_new.html'
+    success_url = reverse_lazy('home')
 
 # update the details with selected profile (class based view)
 class EditView(UpdateView):
@@ -34,45 +30,19 @@ class EditView(UpdateView):
     form_class = DetailForm
     pk_url_kwarg = 'pk'
     template_name = 'edit.html'
-    success_url = reverse_lazy('update_view')  # Adjust the URL name
-                                              # reverse_lazy: This is a function provided by Django that reverses a URL pattern based on its name.
+    success_url = reverse_lazy('update_view')
 
-    def form_valid(self, form):
-        # Get the current instance
-        self.object = self.get_object()
-
-        # Handle image update
-        if form.has_changed():
-            # Handle image update
-            new_image = form.cleaned_data.get('images')
-
-            # Check if a new image is provided
-            if new_image:
-                # Delete the old image if it exists
-                if self.object.images and self.object.images.path:
-                    old_image_path = self.object.images.path
-                    if os.path.exists(old_image_path):
-                        os.remove(old_image_path)
-
-                # Update the images field of the DetailsModel instance with the new image
-                self.object.images = new_image
-
-        # Save the updated instance
-        self.object.save()
-        
-        return super().form_valid(form)
-    
-# update_view will display the update.html page
-class Update_View(ListView):
+# update_view will display the update_success.html page
+class UpdateView(ListView):
     model = DetailsModel
-    template_name = 'update.html'
+    template_name = 'update_success.html'
     context_object_name = 'update_view'
 
 # Delete profile
-class Delete_View(DeleteView):
+class DeleteView(DeleteView):
     model = DetailsModel
     pk_url_kwarg ='pk'
-    template_name = 'delete.html'   
+    template_name = 'delete_confirm.html'   
     success_url = '/'
 
 
